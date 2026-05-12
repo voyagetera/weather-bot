f44675de96914e91fe906c29d9d923315e0d995d
voyagetera <voyage.tera@gmail.com>
2026-05-12 13:56:52 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,14 @@
+import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "ĮČIA_ĮKELK_SAVO_TOKEN"
+TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Vėjo botas veikia 🌬")
-
-async def wind(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("💨 Vėjas: 12 m/s SW (testas)")
+    await update.message.reply_text("🌬 veikia")
 
 app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("wind", wind))
 
 app.run_polling()
