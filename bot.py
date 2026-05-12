8bf3652b9fe40fe99af78253bc47e664c9543226
voyagetera <voyage.tera@gmail.com>
2026-05-12 14:10:01 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,3 @@
 import os
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("TOKEN")
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🌬 veikia")
-
-app = Application.builder().token(TOKEN).build()
-
-app.add_handler(CommandHandler("start", start))
-
-app.run_polling()
+print("TOKEN =", os.getenv("TOKEN"))
